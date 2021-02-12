4ee14a1e02997267c0deea4b82ad4ac53263ebf0
Basil <basilswaggert@gmail.com>
2021-02-12 11:38:06 -0600

    Added all the code.

---
+++ main.js
@@ -0,0 +1,9 @@
+function doService() {
+    var x = document.getElementById("mySelect").value;
+    if(x === "three"){
+        alert("We are sorry, but we only service Minnesota at this time.");
+    } else {
+        alert("We will be happy to assist you!");
+    }
+}
+
