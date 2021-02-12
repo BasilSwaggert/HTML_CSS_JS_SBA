6ab8d6cba01d9c03e07fd397601d4e47730845f3
Basil <basilswaggert@gmail.com>
2021-02-12 11:58:23 -0600

    Added new page

--- main.js
+++ main.js
@@ -6,4 +6,31 @@ function doService() {
         alert("We will be happy to assist you!");
     }
 }
-
+setInterval(showTime, 1000); 
+function showTime() { 
+    var time = new Date(); 
+    var hour = time.getHours(); 
+    var min = time.getMinutes(); 
+    var sec = time.getSeconds(); 
+    am_pm = "AM"; 
+  
+    if (hour > 12) { 
+        hour -= 12; 
+        am_pm = "PM"; 
+    } 
+    if (hour == 0) { 
+        hr = 12; 
+        am_pm = "AM"; 
+    } 
+  
+    hour = hour < 10 ? "0" + hour : hour; 
+    min = min < 10 ? "0" + min : min; 
+    sec = sec < 10 ? "0" + sec : sec; 
+  
+    var currentTime = hour + ":" 
+            + min + ":" + sec + am_pm; 
+  
+    document.getElementById("clock") 
+            .innerHTML = currentTime; 
+} 
+showTime();
